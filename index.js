5de515445e896b9e4e53235a49e5bd52711d23af
kuka <kuancarlos@mail.ru>
2023-07-11 22:09:48 +0500

    jhkj

--- index.js
+++ index.js
@@ -34,11 +34,11 @@ const uploads = multer({ storage });
 
 app.use(express.json());   //add can read .json
 const corsOptions ={
-   origin:'http://localhost:3000',
+   origin:["http://localhost:3000", "https://vercel-lead-off-frontend.vercel.app"],
    credentials:true, //access-control-allow-credentials:true
    optionSuccessStatus:200
    }
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 //app.use(cors());
 app.use(handlers);
 app.use("/api", router);
