e83eadef4f588acce7ce259d819065e1633ead3c
Yamemik <kuancarlos@mail.ru>
2023-07-14 11:19:32 +0500

    refac cors

--- index.js
+++ index.js
@@ -37,14 +37,7 @@ const storage = multer.diskStorage({
 const uploads = multer({ storage });
 
 app.use(express.json());   //add can read .json
-const corsOptions = {
-   origin: ["http://localhost:3000", "https://vercel-lead-off-frontend.vercel.app",
-      "https://frontend-vercel-lead-off.vercel.app"],
-   credentials: true, //access-control-allow-credentials:true
-   optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
-app.options('*', cors());
+app.use(cors());
 app.use(handlers);
 app.use("/api", router);
 app.use('/uploads', express.static('uploads'));
