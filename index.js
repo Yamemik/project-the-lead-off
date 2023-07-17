d98ab2f9c32277b8956d60058cb5fcd31b471622
Yamemik <kuancarlos@mail.ru>
2023-07-17 09:12:52 +0500

    beget settings

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ import fs from 'fs';
 import multer from 'multer';
 import swaggerUi from 'swagger-ui-express';
 
-import { UserController, OrderController, RegionController, CategoryController, RateController, SettingController,
-   NumberOrderController, UkassaController, PaymentController } from './controllers/index.js';
+import {
+   UserController, OrderController, RegionController, CategoryController, RateController, SettingController,
+   NumberOrderController, UkassaController, PaymentController
+} from './controllers/index.js';
 
 import { registerValidation, loginValidation, updateValidation, resentPassValidation } from './validations/AdminValidation.js';
 import { createRegionValidation, createValidationIndexes, createCategoryValidation } from './validations/SettingsValidation.js';
@@ -14,7 +16,9 @@ import { createOrderValidation, updateOrderValidation, findDublicateOrderValidat
 import { checkAuth, checkAuthIsAdmin, handlValidationErrors, handlers } from './utils/index.js';
 
 //connect db
-mongoose.connect('mongodb+srv://admin:admin@cluster0.532y6ot.mongodb.net/lead-off?retryWrites=true&w=majority')
+const string_connect_beget = 'mongodb://admin:Yt%25EPk7W@45.9.41.126:27017/?authMechanism=DEFAULT';
+const string_connect_test = 'mongodb+srv://admin:admin@cluster0.532y6ot.mongodb.net/lead-off?retryWrites=true&w=majority';
+mongoose.connect(string_connect_beget)
    .then(() => console.log('db.....ok!!'))
    .catch((err) => console.log('db ERROR:', err));
 
@@ -23,29 +27,29 @@ const router = express.Router();
 const swaggerFile = JSON.parse(fs.readFileSync('./swagger/output.json'));
 const storage = multer.diskStorage({
    destination: function (req, file, cb) {
-     cb(null, 'uploads')
+      cb(null, 'uploads')
    },
    filename: function (req, file, cb) {
-     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-     cb(null, file.fieldname + '-' + uniqueSuffix)
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
    }
- });
+});
 const uploads = multer({ storage });
 
 app.use(express.json());   //add can read .json
-const corsOptions ={
-   origin:["http://localhost:3000", "https://vercel-lead-off-frontend.vercel.app",
-   "https://frontend-vercel-lead-off.vercel.app"],
-   credentials:true, //access-control-allow-credentials:true
-   optionSuccessStatus:200
-   }
+const corsOptions = {
+   origin: ["http://localhost:3000", "https://vercel-lead-off-frontend.vercel.app",
+      "https://frontend-vercel-lead-off.vercel.app"],
+   credentials: true, //access-control-allow-credentials:true
+   optionSuccessStatus: 200
+}
 app.use(cors(corsOptions));
-app.options('*',cors());
+app.options('*', cors());
 app.use(handlers);
 app.use("/api", router);
 app.use('/uploads', express.static('uploads'));
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(express.static("../www"));
+app.use("/", express.static("../frontend"));
 
 
 //AUTH
@@ -66,7 +70,7 @@ router.patch('/admin/order/:id', checkAuthIsAdmin, updateOrderValidation, handlV
 router.delete('/admin/order', checkAuthIsAdmin, OrderController.removeMany);
 router.delete('/admin/order/:id', checkAuthIsAdmin, OrderController.remove);
 router.post('/admin/order/finddublicate', checkAuthIsAdmin, findDublicateOrderValidation, handlValidationErrors, OrderController.findDublicate);
-router.post('/admin/uploads', checkAuthIsAdmin, uploads.array('file',12), OrderController.cpUpload);
+router.post('/admin/uploads', checkAuthIsAdmin, uploads.array('file', 12), OrderController.cpUpload);
 
 //SETTING
 //region
@@ -119,6 +123,9 @@ router.patch('/user/order/sendCancel/:id', checkAuth, OrderController.sendCancel
 router.get('/admin/numberorder', checkAuthIsAdmin, NumberOrderController.getAllNo);
 router.post('/admin/numberorder', checkAuthIsAdmin, NumberOrderController.createNo);
 
+app.use('*', (req, res) => {
+   res.sendFile("/var/www/frontend/index.html");
+});
 
 //run server
 app.listen(3000, (err) => {
