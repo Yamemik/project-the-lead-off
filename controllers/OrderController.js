844af716e0e60293639db72a98967d88839bebf6
Yamemik <kuancarlos@mail.ru>
2023-08-03 17:10:12 +0500

    refac 1235

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -548,13 +548,16 @@ export const report_order_and_user = async (req, res) => {
          const orders = await OrderModel.find(
             {               
                createdAt: { $gte: req.body.date_begin },
-               createdAt: { $lte: req.body.date_end },               
+               createdAt: { $lte: req.body.date_end },
                is_archive: { $in: req.body.is_archive },
                is_buy: { $in: req.body.is_buy },
                is_sale: { $in: req.body.is_sale },
                is_express: { $in: req.body.is_express },
                is_urgent: { $in: req.body.is_urgent },
-               is_cancel: { $in: req.body.is_cancel }               
+               is_cancel: { $in: req.body.is_cancel },
+               score: { $in: req.body.score },
+               type_buyer: { $in: req.body.type_buyer },
+               type_order: { $in: req.body.type_order },
             }
          )
          .catch((err) => {
@@ -604,13 +607,16 @@ export const report_order_and_user = async (req, res) => {
             {
                user: req.body.user_id,
                createdAt: { $gte: req.body.date_begin },
-               createdAt: { $lte: req.body.date_end },               
+               createdAt: { $lte: req.body.date_end },
                is_archive: { $in: req.body.is_archive },
                is_buy: { $in: req.body.is_buy },
                is_sale: { $in: req.body.is_sale },
                is_express: { $in: req.body.is_express },
                is_urgent: { $in: req.body.is_urgent },
-               is_cancel: { $in: req.body.is_cancel }               
+               is_cancel: { $in: req.body.is_cancel },
+               score: { $in: req.body.score },
+               type_buyer: { $in: req.body.type_buyer },
+               type_order: { $in: req.body.type_order },
             }
          )
          .catch((err) => {
@@ -660,89 +666,124 @@ export const report_order_and_user = async (req, res) => {
 export const report_order_and_cat = async (req, res) => {
    /*
       #swagger.tags = ["Report"]
-      #swagger.summary = 'отчет по заявкам и категориям'
+      #swagger.summary = 'отчет по заявкам и пользователя'
    */
-   try {
-      const orders = await OrderModel.find(
-         {
-            $set: {
-               nomeclature: { $in: req.body.nomeclature },
-               is_urgent: { $in: req.body.is_urgent },
-               price: { $gte: req.body.price_min },
-               price: { $lte: req.body.price_max },
+   if(!req.body.category_id){
+      try {
+         const orders = await OrderModel.find(
+            {               
+               createdAt: { $gte: req.body.date_begin },
+               createdAt: { $lte: req.body.date_end },
                is_archive: { $in: req.body.is_archive },
-               is_sale: { $in: req.body.is_sale },
                is_buy: { $in: req.body.is_buy },
+               is_sale: { $in: req.body.is_sale },
                is_express: { $in: req.body.is_express },
-               is_cancel: { $in: req.body.is_cancel }
+               is_urgent: { $in: req.body.is_urgent },
+               is_cancel: { $in: req.body.is_cancel },
+               score: { $in: req.body.score },
+               type_buyer: { $in: req.body.type_buyer },
+               type_order: { $in: req.body.type_order },
+            }
+         )
+         .catch((err) => {
+            return res.status(404).json({
+               message: 'orders not found'
+            })
+         });
+
+         const count_orders = Object.keys(orders).length;
+         if(count_orders === 0) {
+            return res.json(count_orders);   
+         }
+         let sum_price = 0;
+         let min_price = orders[0].price;
+         let max_price = orders[0].price;        
+
+         for (let order of orders){
+            sum_price += order.price;
+            if(min_price > order.price) {
+               min_price = order.price;
+            } else {
+               if(max_price < order.price){
+                  max_price = order.price;
+               }
             }
          }
-      )
-      .catch((err) => {
-         res.status(404).json({
-            message: 'orders not found'
-         })
-      });
+         const average_sum = sum_price/count_orders;
 
-      res.json(orders);
-   } catch (err) {
-      console.log(err);
-      res.status(500).json({
-         message: "server error"
-      });
-   }
-}
-
-//аналитика
-export const report_count_orders = async (req, res) => {
-   /*
-      #swagger.tags = ["Report"]
-      #swagger.summary = 'count()'
-   */
-  if(!req.body.user_id){
-   try {
-      const orders = await OrderModel.find(
-         {
-            is_archive: { $in: req.body.is_archive },
-            is_buy: { $in: req.body.is_buy },
-            is_cancel: { $in: req.body.is_cancel }         
+         const aggregations = {
+            count_orders: count_orders,
+            sum_price: sum_price,
+            average_sum: average_sum,
+            min_price: min_price,
+            max_price: max_price
          }
-      ).count()
-      .catch((err) => {
-         res.status(404).json({
-            message: 'orders not found'
-         })
-      });
-
-      res.json(orders);
-   } catch (err) {
-      console.log(err);
-      res.status(500).json({
-         message: "server error"
-      });
-   }
-  } else {
-   try {
-      const orders = await OrderModel.find(
-         {
-            user: req.body.user_id,
-            is_archive: { $in: req.body.is_archive },
-            is_buy: { $in: req.body.is_buy },
-            is_cancel: { $in: req.body.is_cancel }
+   
+         res.json({orders,aggregations});
+      } catch (err) {
+         console.log(err);
+         res.status(500).json({
+            message: "server error"
+         });
+      }
+      } else {
+      try {
+         const orders = await OrderModel.find(
+            {
+               nomeclature: req.body.category_id,
+               createdAt: { $gte: req.body.date_begin },
+               createdAt: { $lte: req.body.date_end },
+               is_archive: { $in: req.body.is_archive },
+               is_buy: { $in: req.body.is_buy },
+               is_sale: { $in: req.body.is_sale },
+               is_express: { $in: req.body.is_express },
+               is_urgent: { $in: req.body.is_urgent },
+               is_cancel: { $in: req.body.is_cancel },
+               score: { $in: req.body.score },
+               type_buyer: { $in: req.body.type_buyer },
+               type_order: { $in: req.body.type_order },
+            }
+         )
+         .catch((err) => {
+            res.status(404).json({
+               message: 'orders not found'
+            })
+         });
+   
+         const count_orders = Object.keys(orders).length;
+         if(count_orders === 0) {
+            return res.json(count_orders);   
          }
-      ).count()
-      .catch((err) => {
-         res.status(404).json({
-            message: 'orders not found'
-         })
-      });
+         let sum_price = 0;
+         let min_price = orders[0].price;
+         let max_price = orders[0].price;        
 
-      res.json(orders);
-   } catch (err) {
-      console.log(err);
-      res.status(500).json({
-         message: "server error"
-      });
-   }
-  }
+         for (let order of orders){
+            sum_price += order.price;
+            if(min_price > order.price) {
+               min_price = order.price;
+            } else {
+               if(max_price < order.price){
+                  max_price = order.price;
+               }
+            }
+         }
+         const average_sum = sum_price/count_orders;
+
+         const aggregations = {
+            count_orders: count_orders,
+            sum_price: sum_price,
+            average_sum: average_sum,
+            min_price: min_price,
+            max_price: max_price
+         }
+   
+         res.json({orders,aggregations});
+      } catch (err) {
+         console.log(err);
+         res.status(500).json({
+            message: "server error"
+         });
+      }
+      }
 }
